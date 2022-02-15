c2429587622d362d1b4b78f409aded7539eab93c
wrap-that-potassium <wrap-that-potassium@protonmail.com>
2022-02-15 22:41:45 +0100

    wBAN on Fantom.

--- frontend/src/components/farms/Farm.vue
+++ frontend/src/components/farms/Farm.vue
@@ -332,7 +332,11 @@ export default class Farm extends Vue {
 	addLiquidity() {
 		if (this.value.quoteToken.address) {
 			const otherToken = this.value.quoteToken.address[Farm.ENV_NAME as keyof Address]
-			if (Farm.DEX_URL === 'https://app.sushi.com' || Farm.DEX_URL === 'https://pancakeswap.finance') {
+			if (
+				Farm.DEX_URL === 'https://app.sushi.com' ||
+				Farm.DEX_URL === 'https://pancakeswap.finance' ||
+				Farm.DEX_URL === 'https://spookyswap.finance'
+			) {
 				openURL(`${Farm.DEX_URL}/add/${this.wbanAddress}/${otherToken}`)
 			} else {
 				openURL(`${Farm.DEX_URL}/#/add/${this.wbanAddress}/${otherToken}`)
